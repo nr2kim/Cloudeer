6759aa517301ed9657af124e686bd72758c0d2cd
Kate Kim <kate.kim@autodesk.com>
2018-05-23 22:54:38 -0400

    copy dropbox example; don't know if it will work or not tho

--- src/example.java
+++ src/example.java
@@ -1,18 +1,65 @@
 /**
  * Example from https://github.com/dropbox/dropbox-sdk-java
  */
+package myexample.main;
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.callbacks.DbxGlobalCallbackFactory;
+import com.dropbox.core.v2.callbacks.DbxRouteErrorCallback;
+import com.dropbox.core.v2.callbacks.DbxNetworkErrorCallback;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.util.List;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class Main {
-    private static final String ACCESS_TOKEN = "RGaKeZOu4WAAAAAAAAAbmTZAm_ooJGpWtPUwJqM7RjvlrYMzvIrxjwrEfEgXAuby";
+    private static final String ACCESS_TOKEN = "<ACCESS TOKEN>";
 
-    // can use curl command to obtain access token
-    // curl <a class="vglnk" href="https://api.dropbox.com/1/account/info" rel="nofollow"><span>https</span><span>://</span><span>api</span><span>.</span><span>dropbox</span><span>.</span><span>com</span><span>/</span><span>1</span><span>/</span><span>account</span><span>/</span><span>info</span></a> -H "Authorization:Bearer <YOUR-ACCESS-TOKEN>"
-    // refer : https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/
-    public static void main(String args[]) throws DbxException {
+    public static void main(String args[]) throws DbxException, IOException {
         // Create Dropbox client
-        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial");
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        // Get current account info
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+
+        // Get files and folder metadata from Dropbox root directory
+        ListFolderResult result = client.files().listFolder("");
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                System.out.println(metadata.getPathLower());
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("test.txt")) {
+            FileMetadata metadata = client.files().uploadBuilder("/test.txt")
+                .uploadAndFinish(in);
+        }
+
+        DbxDownloader<FileMetadata> downloader = client.files().download("/test.txt");
+        try {
+            FileOutputStream out = new FileOutputStream("test.txt");
+            downloader.download(out);
+            out.close();
+        } catch (DbxException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
